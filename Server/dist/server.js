e5e717353982c1c02af7605d3abb7309115f2c8d
Pottassiuw <pottassiu@proton.me>
2025-09-11 14:05:42 -0300

    Files//Adding the base structure

---
+++ Server/dist/server.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function a(n) {
+    return n * 2;
+}
+console.log(a(4));
---
+++ Server/src/server.ts
@@ -0,0 +1,5 @@
+function a(n:number) {
+    return n*2
+}
+
+console.log(a(4))
