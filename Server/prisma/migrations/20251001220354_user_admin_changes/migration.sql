a50e868eb5d9a16086de741f57c488f928bd8eb9
Pottassiuw <pottassiu@proton.me>
2025-10-18 00:12:53 -0300

    Removed Server folder because now it's another repository

--- Server/prisma/migrations/20251001220354_user_admin_changes/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Usuario" ADD COLUMN     "admin" BOOLEAN NOT NULL DEFAULT false;
--- Server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import app from "./app";
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`Escutando a porta ${port}`);
-});
